8f99bca679405501ce2e38be653d04b34748cd7d
berthin <btn.torres@gmail.com>
2023-10-08 19:42:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 setup(
     name="cmakelang-precommit-dummy",
     version="0.0.0",
-    install_requires=["cmakelang==pre-release"]
+    install_requires=["git+https://github.com/berthin/cmake_format@pre-release#egg=cmakelang"]
 )
